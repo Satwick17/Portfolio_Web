0f27d8619773ab36329f3f2fb881b459363eaf7a
name <satwickdeep2001@gmail.com>
2024-07-18 20:23:43 +0530

    Project functionality added

--- Dashboard/dashboard/src/pages/special_Components/AddProject.jsx
+++ Dashboard/dashboard/src/pages/special_Components/AddProject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -11,8 +11,63 @@ import { Link } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import SpecialLoadingButton from "./SpecialLoadingButton";
+import {
+  addNewProject,
+  clearAllProjectErrors,
+  getAllProjects,
+  resetProjectSlice,
+} from "@/store/slices/projectSlice";
 
 const AddProject = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [projectBanner, setProjectBanner] = useState("");
+  const [projectBannerPreview, setProjectBannerPreview] = useState("");
+  const [gitRepoLink, setGitRepoLink] = useState("");
+  const [projectLink, setProjectLink] = useState("");
+  const [technologies, setTechnologies] = useState("");
+  const [stack, setStack] = useState("");
+  const [deployed, setDeployed] = useState("");
+
+  const handleSvg = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setProjectBannerPreview(reader.result);
+      setProjectBanner(file);
+    };
+  };
+
+  const { loading, error, message } = useSelector((state) => state.project);
+  const dispatch = useDispatch();
+
+  const handleAddNewProject = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("discription", description);
+    formData.append("gitRepoLink", gitRepoLink);
+    formData.append("projectLink", projectLink);
+    formData.append("projectBanner", projectBanner);
+    formData.append("technologies", technologies);
+    formData.append("deployed", deployed);
+    formData.append("stack", stack);
+    dispatch(addNewProject(formData));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearAllProjectErrors());
+    }
+    if (message) {
+      toast.success(message);
+      dispatch(resetProjectSlice());
+      dispatch(getAllProjects());
+    }
+  }, [dispatch, loading, error, message]);
+
   return (
     <>
       <div className="flex mt-7 justify-center items-center min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-14">
@@ -35,7 +90,7 @@ const AddProject = () => {
                       <input
                         type="text"
                         className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="MERN STACK PORTFOLIO"
+                        placeholder="Project Title"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                       />
@@ -49,7 +104,7 @@ const AddProject = () => {
                   <div className="mt-2">
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
                       <Textarea
-                        placeholder="Feature 1. Feature 2. Feature 3."
+                        placeholder="Description"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                       />
@@ -213,7 +268,7 @@ const AddProject = () => {
             </div>
           </div>
 
-          <div className="mt-6 flex items-center justify-end gap-x-6">
+          <div className="mt-6 flex items-center justify-center gap-x-6">
             {loading ? (
               <SpecialLoadingButton
                 content={"ADDING NEW PROJECT"}
